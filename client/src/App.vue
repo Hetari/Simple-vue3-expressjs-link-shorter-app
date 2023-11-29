f5cad7be01b79e0e6f7fc0e90ebc2c72bfe2887a
Hetari <hetari4py@gmail.com>
2023-11-29 11:20:25 +0300

    setup my vue project with bootstrap

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,11 +3,11 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -26,5 +26,5 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+// Import Bootstrap
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-createApp(App).use(router).mount('#app')
+// Set up the app
+const app = createApp(App);
+app.use(router);
+
+// Set up Bootstrap
+// app.use(BootstrapVue);
+// app.use(IconsPlugin);
+
+// Mount the app
+app.mount("#app");
